c4f5d40673661f79bd5afc045b4c11aac36b35d1
Benjamin Chodoroff <ben@theworkdept.com>
2014-11-14 15:53:19 -0500

    push secondary into place

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -130,7 +130,7 @@
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-3 col-sm-push-6" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
