3deb3690eef95884ccf77b8132d8692f67dc6ebd
Benjamin Chodoroff <ben@theworkdept.com>
2014-11-16 22:07:05 -0500

    refs #4311: align menu with left border of content area

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@
 Drupal.behaviors.amphome_menu = {
   attach: function(context, settings) {
     var $grandchildren = $(".dropdown-menu .dropdown-menu", context);
+    var logo_width = $('#navbar > div > div.col.col-xs-12.col-md-2').outerWidth();
 
     $grandchildren.each(function() {
       var $grandparent = $(this).parent().closest('.dropdown-menu');
       var container_width = $(this).closest('.container').css('width');
+      var left = logo_width + $grandparent.parent().position().left;
       $grandparent
         .addClass('grandparent')
-        .css({width: container_width});
+        .css({
+          width: container_width,
+          position: 'absolute',
+          left: '-' + left + 'px'
+        });
     });
   }
 };
