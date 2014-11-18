98fa6c6e42741bcdb43225fa2829a8ab2ad460fe
Benjamin Chodoroff <ben@theworkdept.com>
2014-11-18 10:47:04 -0500

    make search form smaller

--- templates/bootstrap-search-form-wrapper.func.php
+++ templates/bootstrap-search-form-wrapper.func.php
@@ -7,11 +7,11 @@
 /**
  * Theme function implementation for bootstrap_search_form_wrapper.
  */
-function bootstrap_bootstrap_search_form_wrapper($variables) {
+function amphome_bootstrap_search_form_wrapper($variables) {
   $output = '<div class="input-group">';
-  $output .= $variables['element']['#children'];
+  $output .= str_replace('form-control form-text', 'form-control form-text input-sm', $variables['element']['#children']);
   $output .= '<span class="input-group-btn">';
-  $output .= '<button type="submit" class="btn btn-primary">' . _bootstrap_icon('search', t('Search')) . '</button>';
+  $output .= '<button type="submit" class="btn btn-sm btn-primary">' . _bootstrap_icon('search', t('Search')) . '</button>';
   $output .= '</span>';
   $output .= '</div>';
   return $output;
