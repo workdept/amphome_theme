4950aa529270e863f8671f55d1540421b654329b
Benjamin Chodoroff <ben@theworkdept.com>
2014-12-09 10:07:02 -0500

    better test for term image

--- templates/node.vars.php
+++ templates/node.vars.php
@@ -2,10 +2,10 @@
 
 function amphome_preprocess_node(&$vars, $hook) {
   if ($vars['type'] == 'news_item') {
-    if (isset($vars['field_news_term']) && count($vars['field_news_term']) === 1 && isset($vars['field_news_term'][0]['taxonomy_term']->field_term_image)) {
+    if (isset($vars['field_news_term']) && count($vars['field_news_term']) === 1 && isset($vars['field_news_term'][0]['taxonomy_term']->field_term_image) && count($vars['field_news_term'][0]['taxonomy_term']->field_term_image) === 1) {
       $field_news_term = $vars['field_news_term'][0];
     }
-    if (isset($vars['field_news_term']) && count($vars['field_news_term']['und']) === 1 && isset($vars['field_news_term']['und'][0]['taxonomy_term']->field_term_image)) {
+    if (isset($vars['field_news_term']) && count($vars['field_news_term']['und']) === 1 && isset($vars['field_news_term']['und'][0]['taxonomy_term']->field_term_image) && count($vars['field_news_term']['und'][0]['taxonomy_term']->field_term_image) === 1) {
       $field_news_term = $vars['field_news_term']['und'][0];
     }
     if (isset($field_news_term)) {
