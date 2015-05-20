a8b357e76c604e3cb1ba107b6827e9e08f82ecef
Meghan Sweet <meghan.sweet@gmail.com>
2015-05-20 12:41:27 -0700

    Adding mmenu trigger button to page.tpl.php

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -88,13 +88,12 @@
           <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
         <?php endif; ?>
 
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <a href="#mmenu_left" class="navbar-toggle">
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
-        </button>
+        </a>
       </div>
     </div>
 
