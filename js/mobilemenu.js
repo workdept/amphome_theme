bf1ffb2a0941ce6fbcb20f9a7edb5304fc69fd3b
Meghan Sweet <meghan.sweet@gmail.com>
2016-02-26 13:57:01 -0800

    Fixing JS bug in mobile menu

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -6,7 +6,7 @@
             //Toggle the sub menu navigation
             $("#block-block-2 button").click(function() {
                 $('#block-block-2').toggleClass("closed");
-                $("section.block-menu-block nav").toggle();
+                $(".main-container section.block-menu-block nav").toggle();
                 $('#block-block-2 span').toggleClass("hidden");
             });
 
